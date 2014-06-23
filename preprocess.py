4e51fbd21a7b35f54288e2f03efe6e3ddd9e5f86
daniel <daniel.kao.95@gmail.com>
2014-06-23 10:50:50 -0400

    Added spaces lol according to PEP8 yo

--- preprocess.py
+++ preprocess.py
@@ -46,21 +46,21 @@ class Course:
         pass
 
     def concatenate(self):
-        return str(self.subject)+str(self.catalog)
+        return str(self.subject) + str(self.catalog)
 
     def printCourse(self):
-        print str(self.subject)+" "+str(self.catalog)+" ,"+str(self.credits)
+        print str(self.subject) + " " + str(self.catalog) + " ," + str(self.credits)
 
 def findFiles(location, extension):
 
     ''' #Searches a location for all files that have a specific extension
         #Used to return all the excel workbooks (xls) locations to the main function
     '''
-    workbooksList=[]    #delcaration, stores the file locations of all the workbooks
+    workbooksList = []    #delcaration, stores the file locations of all the workbooks
 
     for filename in os.listdir(location): #goes through every file in the folder
-        if filename[-3:]==extension:
-            workbooksList.append(location+"\\"+filename)  #adds the file if the last 3 char are "xls"
+        if filename[-3:] == extension:
+            workbooksList.append(location + "\\" + filename)  #adds the file if the last 3 char are "xls"
 
     return workbooksList
 
@@ -77,7 +77,7 @@ def checkTableExist(tableName):
 
     temp = c.fetchone()
 
-    if temp==(1,):
+    if temp == (1,):
         return True
     else:
         return False
@@ -85,49 +85,49 @@ def checkTableExist(tableName):
 
 def getStudents(headingsNeeded, currentSheet):
 
-    headingsLocation=[]
+    headingsLocation = []
 
     for i in range(len(headingsNeeded)):
         headingsLocation.append(dbFormat.findCol(currentSheet,headingsNeeded[i]))
 
-    studList=[Student() for i in range(currentSheet.nrows)]         #create the array of students
+    studList = [Student() for i in range(currentSheet.nrows)]         #create the array of students
 
     for row in range(currentSheet.nrows):
         '''parse the data of each row into a Student object, which is in the array'''
-        values=[]
+        values = []
 
         for dataCol in headingsLocation:
             
             values.append(currentSheet.cell(row,dataCol).value)
 
-        studList[row].studID=(values[0])
-        studList[row].program=values[1]
-        studList[row].proj_level=(values[2])
-        studList[row].plan=values[3]
-        studList[row].plan2=""
-        studList[row].plan3=""
+        studList[row].studID = (values[0])
+        studList[row].program = values[1]
+        studList[row].proj_level = (values[2])
+        studList[row].plan = values[3]
+        studList[row].plan2 = ""
+        studList[row].plan3 = ""
 
-    popRows=[]
+    popRows = []
 
     popRows.extend(studentProcessing.findImproperStudent(studList))   #finds the headings and whitespaces and flags the index
 
     for i in studentProcessing.findTriple(studList):
        '''finds the triples (accounts for con ed, who MAY have B.ED + double major for their plans, 3 rows)'''
-       studList[i+2].plan3=studList[i].plan
+       studList[i + 2].plan3 = studList[i].plan
     popRows.extend(studentProcessing.findTriple(studList))            #flags the index for triples
 
     for i in reversed(popRows):                     #when iterating in reverse, the wrong things do not get popped off the list
         studList.pop(i)                             #pop off headings/whitespaces and triples
 
-    popRows=[]                                      #resets the list so we can add duplicate indexes
+    popRows = []                                      #resets the list so we can add duplicate indexes
 
     for i in studentProcessing.findDuplicate(studList):               #finds the duplicates and adds the indices to the popRows list
-        studList[i+1].plan2=studList[i].plan        #before the duplicate is popped, the second plan is added to the data row
+        studList[i + 1].plan2 = studList[i].plan        #before the duplicate is popped, the second plan is added to the data row
     popRows.extend(studentProcessing.findDuplicate(studList))
 
     for i in reversed(popRows):                     #when iterating in reverse, the wrong things do not get popped off the list
         studList.pop(i)
-    popRows=[]
+    popRows = []
 
     for i in range(len(studList)):
         studList[i].convert()                       #finalize input data
@@ -139,19 +139,19 @@ def getCourseInfo(neededHeadings,currentSheet):
         in the database, for the course table
     '''
 
-    headingsLocation=[]
+    headingsLocation = []
 
     for i in range (len(neededHeadings)):
         headingsLocation.append(dbFormat.findCol(currentSheet,neededHeadings[i]))
 
-    courseInfo=Course()     #initialize the course object
+    courseInfo = Course()     #initialize the course object
 
-    values=[]
+    values = []
 
     for dataCol in headingsLocation:
         values.append(currentSheet.cell(2,dataCol).value)
-    courseInfo.subject=(values[0])
-    courseInfo.catalog=values[1]
+    courseInfo.subject = (values[0])
+    courseInfo.catalog = values[1]
 
     return courseInfo
 
@@ -162,14 +162,14 @@ def getCourseInfo(neededHeadings,currentSheet):
 ''' Initialize all the connections to SQLite
     Configure database stuff
 '''
-cdLocation=os.getcwd()
-dbLocation=cdLocation+"/testv2.db" #"""@@@@@@@@@@@@@@@@@@@ TAKE OUT THE HARD CODE"""
-rawDataLocation=cdLocation+"\\data"
+cdLocation = os.getcwd()
+dbLocation = cdLocation + "/testv2.db" #"""@@@@@@@@@@@@@@@@@@@ TAKE OUT THE HARD CODE"""
+rawDataLocation = cdLocation + "\\data"
 
 os.remove(dbLocation) #"""@@@@@@@@@@ REMOVES THE DB EACH TIME THIS RUNS, FOR TESTING PURPOSES"""
 
-conn=sqlite3.connect(dbLocation)
-c=conn.cursor()
+conn = sqlite3.connect(dbLocation)
+c = conn.cursor()
 
 c.execute("PRAGMA foreign_keys=ON;")    #enables foreign keys
 
@@ -178,37 +178,37 @@ c.execute("PRAGMA foreign_keys=ON;")    #enables foreign keys
 '''Initialize the Excel workbook connections for reading using xlrd module
     Find all xls files and then store the location and addresses of all of them
 '''
-excelExtension="xls"
-filesList=findFiles(rawDataLocation,excelExtension)
+excelExtension = "xls"
+filesList = findFiles(rawDataLocation,excelExtension)
 
-wbData=[]
-sheetAddress=[]
+wbData = []
+sheetAddress = []
 for i in range(len(filesList)):
-    wbData.append(xlrd.open_workbook(filesList[i],on_demand=True))
+    wbData.append(xlrd.open_workbook(filesList[i],on_demand = True))
     sheetAddress.append(wbData[i].sheet_by_index(0)) #will access and store the location of the FIRST sheet in each workbook
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Preprocess the excel columns from text to float
-preprocessHeadings=["Student ID","Proj Level","Term"]   #these are the headings of columns that are floats and need to be made into floats
+preprocessHeadings = ["Student ID","Proj Level","Term"]   #these are the headings of columns that are floats and need to be made into floats
 
-macroLocation=cdLocation+"\\excel\\intToFloat.xlsm" #"""@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ change to make it dynamic"""
+macroLocation = cdLocation + "\\excel\\intToFloat.xlsm" #"""@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ change to make it dynamic"""
 
-xl=win32com.client.Dispatch("Excel.Application")    #accesses the excel application
-xl.Visible=1
+xl = win32com.client.Dispatch("Excel.Application")    #accesses the excel application
+xl.Visible = 1
 
-wbList=[]
+wbList = []
 
 for i in range(len(filesList)):
     wbList.append(0)
-    wbList[i]=xl.Workbooks.Open(filesList[i]) #open all the workbooks that need to be changed
+    wbList[i] = xl.Workbooks.Open(filesList[i]) #open all the workbooks that need to be changed
 
-    preprocessHeadingsLocation=dbFormat.findHeadingsList(preprocessHeadings,sheetAddress[i])
+    preprocessHeadingsLocation = dbFormat.findHeadingsList(preprocessHeadings,sheetAddress[i])
     #this only uses one set of table headings so if the table heading columns are different, there will be   problem
 
     for j in range(len(preprocessHeadingsLocation)):
-        preprocessHeadingsLocation[j]=preprocessHeadingsLocation[j]+1   #the table headings must be incremented by 1 so the macro below can process the correct heading
+        preprocessHeadingsLocation[j] = preprocessHeadingsLocation[j] + 1   #the table headings must be incremented by 1 so the macro below can process the correct heading
 
-wbPre=xl.Workbooks.Open(macroLocation)              #open the workbook containing the macro
+wbPre = xl.Workbooks.Open(macroLocation)              #open the workbook containing the macro
 
 for k in range(len(preprocessHeadingsLocation)):    #change every column effected heading in each excel workbook
     xl.Application.Run("makeTexttoFloat",preprocessHeadingsLocation[k])
@@ -221,7 +221,7 @@ wbPre.Save()            #close and save the excel workbook with macros
 wbPre.Close(True)
 
 xl.Application.Quit()   #killing the dispatch
-xl.Visible=0            #need to make it not visible
+xl.Visible = 0            #need to make it not visible
 del (xl)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -230,37 +230,37 @@ del (xl)
 #students table
 #determining headings from excel and making into table headings for sql [hardcoded but will make into dropdown input for UI]
 
-studentTableHeadings=["Student ID","Program","Proj Level","Plan"]
-maxCourses=10    #number of courses that can be enrolled in (should be 5 for A&S for one term, 10 for a year)<<<LOOK INTO IT
+studentTableHeadings = ["Student ID","Program","Proj Level","Plan"]
+maxCourses = 10    #number of courses that can be enrolled in (should be 5 for A&S for one term, 10 for a year)<<<LOOK INTO IT
 
 #c.execute("DROP TABLE students")
 if checkTableExist("students") is False:
-    dataTypes=["INTEGER NOT NULL UNIQUE","TEXT NOT NULL","INTEGER NOT NULL","TEXT NOT NULL"]    #where student_id is a unique field
+    dataTypes = ["INTEGER NOT NULL UNIQUE","TEXT NOT NULL","INTEGER NOT NULL","TEXT NOT NULL"]    #where student_id is a unique field
 
-    studentSQLHeadings=dbFormat.generateHeading(sheetAddress[0],studentTableHeadings,dataTypes)
+    studentSQLHeadings = dbFormat.generateHeading(sheetAddress[0],studentTableHeadings,dataTypes)
 
-    studentSQLHeadings=studentSQLHeadings+", plan2 TEXT, plan3 TEXT"  #amend the headings to add the second plan column (for double majors)
+    studentSQLHeadings = studentSQLHeadings + ", plan2 TEXT, plan3 TEXT"  #amend the headings to add the second plan column (for double majors)
 
-    c.execute("CREATE TABLE students(stud_id INTEGER PRIMARY KEY,"+studentSQLHeadings+")")
+    c.execute("CREATE TABLE students(stud_id INTEGER PRIMARY KEY," + studentSQLHeadings + ")")
 
     for i in range(maxCourses):
-        c.execute("ALTER TABLE students ADD COLUMN course"+str(i+1)+" INTEGER REFERENCES courses(course_id);")
+        c.execute("ALTER TABLE students ADD COLUMN course" + str(i + 1) + " INTEGER REFERENCES courses(course_id);")
 
 #course table
-courseTableHeadings=["Subject","Catalog Number"]    #catalog number must be null because of suffix letters e.g. PHYG 214A
+courseTableHeadings = ["Subject","Catalog Number"]    #catalog number must be null because of suffix letters e.g. PHYG 214A
 
 #c.execute("DROP TABLE courses;")
 if checkTableExist("courses") is False:
-    dataTypes=["TEXT NOT NULL","TEXT NOT NULL"]
+    dataTypes = ["TEXT NOT NULL","TEXT NOT NULL"]
 
-    headingsLocation=[]
+    headingsLocation = []
     for i in range (len(courseTableHeadings)):
         headingsLocation.append(dbFormat.findCol(sheetAddress[0],courseTableHeadings[i]))
 
-    courseSQLHeadings=dbFormat.generateHeading(sheetAddress[0],courseTableHeadings,dataTypes)
+    courseSQLHeadings = dbFormat.generateHeading(sheetAddress[0],courseTableHeadings,dataTypes)
     print courseSQLHeadings
 
-    c.execute("CREATE TABLE courses(course_id INTEGER PRIMARY KEY,"+courseSQLHeadings+")")
+    c.execute("CREATE TABLE courses(course_id INTEGER PRIMARY KEY," + courseSQLHeadings + ")")
 
 ###enrollments table
 ##
@@ -279,6 +279,7 @@ if checkTableExist("courses") is False:
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Pulling student data
+
 ''' Each student list from a workbook is appended to the master student list (studentLists).
     Thus, each item in that array is an array itself, making studentLists a multi-dimensional
     array.
@@ -288,11 +289,11 @@ if checkTableExist("courses") is False:
     are the same. e.g. studentLists[2] will correspond to courseLists[2]
 '''
 
-studentTableHeadings=["Student ID","Program","Proj Level","Plan"]
-courseTableHeadings=["Subject","Catalog Number"] 
-studentLists=[] #studentLists must be reset each time
-courseLists=[]
-courseNames=[]
+studentTableHeadings = ["Student ID","Program","Proj Level","Plan"]
+courseTableHeadings = ["Subject","Catalog Number"] 
+studentLists = [] #studentLists must be reset each time
+courseLists = []
+courseNames = []
 
 
 for i in range(len(sheetAddress)):

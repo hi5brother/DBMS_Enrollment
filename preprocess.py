fe89fc47fe744732fc5bd3f8adec55428f727c0f
daniel <daniel.kao.95@gmail.com>
2014-07-15 16:13:33 -0400

    added credits to DB, win32com.client not working

--- preprocess.py
+++ preprocess.py
@@ -15,6 +15,7 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import os
+import sys
 import sqlite3  #SQLite API
 import xlrd #reading xls files
 import xlwt #writing xls files
@@ -24,6 +25,10 @@ import win32com.client  #for executing excel macros
 
 import studentProcessing        #module that deletes whitespace, double/triple majors when processing each spreadsheet
 import dbFormat                 #formats the tables for the database
+
+
+sys.path.append(os.getcwd() + '/testing/UI')    #adding the UI modules to the path
+import creditsInput
 
 class Student:
     def __init__(self):#,studID,program,proj_level,plan1,plan2,total_credits):
@@ -92,6 +97,8 @@ def getStudents(headingsNeeded, currentSheet):
 
     studList = [Student() for i in range(currentSheet.nrows)]         #create the array of students
 
+    print str(currentSheet) + " " + str(currentSheet.nrows) #@@@@@@@@@@@@@@@@@@@@@@
+
     for row in range(currentSheet.nrows):
         '''parse the data of each row into a Student object, which is in the array'''
         values = []
@@ -106,16 +113,19 @@ def getStudents(headingsNeeded, currentSheet):
         studList[row].plan = values[3]
         studList[row].plan2 = ""
         studList[row].plan3 = ""
+        #print type(studList[row].studID)
 
     popRows = []
 
     popRows.extend(studentProcessing.findImproperStudent(studList))   #finds the headings and whitespaces and flags the index
-
+    print popRows
     for i in studentProcessing.findTriple(studList):
        '''finds the triples (accounts for con ed, who MAY have B.ED + double major for their plans, 3 rows)'''
        studList[i + 2].plan3 = studList[i].plan
     popRows.extend(studentProcessing.findTriple(studList))            #flags the index for triples
 
+    #print str(currentSheet) + str(len(studList)) #@@@@@@@@@@@@@@@@@@
+    print popRows
     for i in reversed(popRows):                     #when iterating in reverse, the wrong things do not get popped off the list
         studList.pop(i)                             #pop off headings/whitespaces and triples
 
@@ -164,7 +174,7 @@ def getCourseInfo(neededHeadings,currentSheet):
 '''
 cdLocation = os.getcwd()
 dbLocation = cdLocation + "\\testv2.db" #"""@@@@@@@@@@@@@@@@@@@ TAKE OUT THE HARD CODE"""
-rawDataLocation = cdLocation + "\\data"
+rawDataLocation = cdLocation + "\\full_data"
 
 os.remove(dbLocation) #"""@@@@@@@@@@ REMOVES THE DB EACH TIME THIS RUNS, FOR TESTING PURPOSES"""
 
@@ -211,6 +221,7 @@ for i in range(len(filesList)):
 wbPre = xl.Workbooks.Open(macroLocation)              #open the workbook containing the macro
 
 for k in range(len(preprocessHeadingsLocation)):    #change every column effected heading in each excel workbook
+    print preprocessHeadingsLocation[k]
     xl.Application.Run("makeTexttoFloat",preprocessHeadingsLocation[k])
 
 for i in range(len(filesList)):
@@ -258,7 +269,7 @@ if checkTableExist("courses") is False:
         headingsLocation.append(dbFormat.findCol(sheetAddress[0],courseTableHeadings[i]))
 
     courseSQLHeadings = dbFormat.generateHeading(sheetAddress[0],courseTableHeadings,dataTypes)
-    print courseSQLHeadings
+    #print courseSQLHeadings
 
     c.execute("CREATE TABLE courses(course_id INTEGER PRIMARY KEY," + courseSQLHeadings + ")")
 
@@ -291,7 +302,7 @@ if checkTableExist("courses") is False:
 
 studentTableHeadings = ["Student ID","Program","Proj Level","Plan"]
 courseTableHeadings = ["Subject","Catalog Number"]
-studentLists = [] #studentLists must be reset each time
+studentLists = []
 courseLists = []
 courseNames = []
 
@@ -300,19 +311,14 @@ for i in range(len(sheetAddress)):
     courseLists.append(getCourseInfo(courseTableHeadings,sheetAddress[i]))
     courseNames.append(courseLists[i].concatenate())
 
-
 c.execute("ALTER TABLE courses ADD course_code TEXT;")  #adding the column with the concatenated course code
 for i in range(len(sheetAddress)):
     c.execute("INSERT INTO courses(subject,catalog_number,course_code) VALUES(?,?,?);",(courseLists[i].subject,courseLists[i].catalog,courseNames[i]))
 
-    for i in range(len(sheetAddress)):
-
-        studentLists.append(getStudents(studentTableHeadings,sheetAddress[i]))
-        courseLists.append(getCourseInfo(courseTableHeadings,sheetAddress[i]))
+    studentLists.append(getStudents(studentTableHeadings,sheetAddress[i]))
 
     for stud in studentLists[i]: #OR IGNORE will only input the record if it is unique (does not show the error message)
         c.execute("INSERT OR IGNORE INTO students (student_id,program,proj_level,plan,plan2,plan3) VALUES(?,?,?,?,?,?);",(stud.studID,stud.program,stud.proj_level,stud.plan,stud.plan2,stud.plan3))
-        #stud.printValues()
 
     c.execute("SELECT course_id FROM courses WHERE course_id=(SELECT MAX(course_ID) FROM courses);") #finds the course_id of the most recently entered course list from the courses table
 
@@ -329,14 +335,31 @@ for i in range(len(sheetAddress)):
     '''
     c.execute("SELECT * FROM students")
     val=c.fetchall()
+
     for stud in studentLists[i]: #goes through all the students in current course list
         for row in val: #goes through all rows of the table
             for i in reversed(range(1,maxCourses+1)): #accesses the row's course values, course 1 thru maxCourses
                 if row[len(row)-i] is None and stud.studID==row[1]: #must check if the student is enrolled in the current course
-                    c.execute("Update students SET course"+str(maxCourses+1-i)+"=? WHERE student_id=?;",(str(courseNum),row[1]))
+                    c.execute("UPDATE students SET course"+str(maxCourses+1-i)+"=? WHERE student_id=?;",(str(courseNum),row[1]))
                     break
+
+
+'''Input the credits using the entry widget (creditsInput module) and then updates the database
+'''
+
+c.execute("ALTER TABLE courses ADD credits FLOAT;")
+c.execute("SELECT course_code FROM courses;")   #grabs names of all the courses
+allCourses = c.fetchall()
+
+creditsList = creditsInput.runApp(allCourses) #initializes the entry widget to input credits data
+
+for credit in creditsList:
+    c.execute("UPDATE courses SET credits = ?;",(credit,))   #adds to each course record the number of credits
+
+
+# for stud in studentLists[0]:
+#     stud.printValues()
+
 
 conn.commit()
 conn.close()
-
-

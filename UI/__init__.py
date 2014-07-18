4bd7532e844838cf8f50cac4822b0585c9b752d1
daniel <daniel.kao.95@gmail.com>
2014-07-18 16:23:00 -0400

    make UI into a module' lo a

---
+++ UI/__init__.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:        __init__ for the UI module 
+
+# Purpose:		UI module contains all the tk widgets
+#
+# Author:      DBMS
+#
+# Created:     18/07/2014
+# Copyright:   (c) DBMS 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------

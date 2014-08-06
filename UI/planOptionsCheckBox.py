cb16d48134bee180d3b10da9b534dfe72043ace2
daniel <daniel.kao.95@gmail.com>
2014-08-06 16:18:12 -0400

    init commit that asks for BIU, prog weight values

--- UI/planOptionsCheckBox.py
+++ UI/planOptionsCheckBox.py
@@ -2,6 +2,8 @@
 # Name:        checkbox that allows user to select multiple options
 #				returns a list of all the selected checkboxes 
 #				e.g. ['BCHM','BIOL']
+#				
+#				includes an app with a scroll bar too
 #
 # Purpose:		
 #
@@ -13,13 +15,14 @@
 #-------------------------------------------------------------------------------
 
 from Tkinter import *
-text = ["BCHM", "LISC","NURS"]
+text = range(1,10)
 
 class CheckBoxApp:
 	def __init__(self,parent,optionsList):
 		self.parent = parent
 		self.container = Frame(parent)
-		self.container.pack()
+		self.container.grid(sticky=(N,S,E,W))
+
 		self.initialize(optionsList)
 
 	def initialize(self,optionsList):
@@ -27,6 +30,9 @@ class CheckBoxApp:
 		self.txtBox = Label(self.container, text = "Please select the required plan breakdowns.")
 		self.txtBox.config(width = 40, height = 2)
 		self.txtBox.grid(row = 0, column = 0)
+
+		self.yScroll = Scrollbar(self.container, orient = VERTICAL)
+		self.yScroll.grid(row = 0, column = 1, sticky = N + S)
 
 		self.optionsList = optionsList
 		self.var = {}
@@ -53,10 +59,60 @@ class CheckBoxApp:
 	def quit(self):
 		self.parent.destroy()
 
+class CheckBoxScrollingApp():
+	'''Adds a scrolling bar to the window. The scrollbar is a part of the textbox frame, since
+		checkbox frames can't have a scrollbar
+	'''
+	def __init__(self,parent,optionsList):
+		self.parent = parent
+		self.initialize(optionsList)
+
+	def initialize(self,optionsList):
+
+		self.vsb = Scrollbar(orient = "vertical")
+
+		tempHeight = (lambda x: 20 if x > 20 else x) (len(optionsList))	#make the length of the box dynamic
+		defaultbg = self.parent.cget('bg')	#set default colour
+		self.text = Text(self.parent, width = 30, height = tempHeight, bg = defaultbg,
+							yscrollcommand = self.vsb.set)
+
+		self.vsb.config(command = self.text.yview)		#scrollbar configuration
+		self.vsb.pack(side = "right", fill = "y")
+		self.text.pack(side = "top", fill = "both", expand = True)
+
+		self.optionsList = optionsList
+		self.var = {}
+		for option in optionsList:
+			self.var[option] = StringVar()
+			self.checkButton = Checkbutton(text = option, variable = self.var[option], 
+										onvalue = option, offvalue = '', justify = RIGHT)
+			self.text.window_create("end", window = self.checkButton)
+			self.text.insert("end", "\n")
+
+		self.infoBox = Label(self.parent, text = "Please select all the required plan breakdowns.")
+		self.infoBox.config(width = 40, height = 2)
+		self.infoBox.pack(side = "top")
+
+		self.subButton = Button(self.parent)
+		self.subButton['text'] = 'Submit'
+		self.subButton.pack(side = "bottom")
+		self.subButton['command'] = self.submit
+
+	def submit(self):
+		self.data = []
+		for option in self.optionsList:
+			if self.var[option].get() != '':		#make sure the option is not a blank
+				self.data.append(self.var[option].get())
+
+		self.parent.destroy()
+
+	def quit(self):
+		self.parent.destroy()
+
 def runApp(text):
 	root = Tk()
 	root.title("DBMS Enrollment Plan Select")
-	app = CheckBoxApp(root,text)
+	app = CheckBoxScrollingApp(root,text)
 	root.mainloop()
 
 	try:
@@ -67,7 +123,19 @@ def runApp(text):
 	except AttributeError:		#if the window was closed 
 		return "Data Error: No data was input"
 
+def runScrollingApp(text):
+	root = Tk()
+	root.title("DBMS Enrollment Plan Select")
+	app = CheckBoxScrollingApp(root,text)
+	root.mainloop()
 
+	try:
+		if app.data == []:		#if submit was hit with nothing chosen
+				return "User Error: No data was input"
+		return app.data 		#returns all the data if it is correct
+	
+	except AttributeError:		#if the window was closed 
+		return "Data Error: No data was input"
 
 
 if __name__ == '__main__':
---
+++ updateConstants.py
@@ -0,0 +1,225 @@
+#-------------------------------------------------------------------------------
+# Name:       	Update Constants
+# Purpose:      access the database and pull the constants data
+#				
+#
+# Author:      DBMS
+#
+# Created:     01/08/2014
+# Copyright:   (c) DBMS 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import extractData as data
+import UI
+
+def checkError(funcOutput):     #returns false is there is an error, returns true if no error exists
+    '''Checks the output of the function to see if an error message was returned
+    '''
+
+    if 'Error' in funcOutput:       #sees if the output returns an error
+        return False
+    else:                       #output does not return an error
+        return True
+
+def checkBack(funcOutput):
+	'''Checks if the BACK button was selected
+	'''
+
+	if 'BACK' in funcOutput:		#Checks if the output shows "QUIT"
+		return True 				#this will mean 
+	else:
+		return False
+
+
+def inputCredits(c, inputStage):
+    
+        
+    c.execute("SELECT course_code, term FROM courses;") #grabs names of all the courses
+    allCourses = c.fetchall()
+
+    courseDisplayName = []
+
+    for i in range(len(allCourses)):
+        #first element [i][0] is the course code, the second [i][1] is the term that the course is in
+        courseDisplayName.append(allCourses[i][0] + " - Term: " + str(allCourses[i][1]))  
+    
+    creditsList = UI.creditsInput.runApp(courseDisplayName)    #initializes the entry widget to input credits data
+    while not checkError(creditsList):                  #do while loop that repeats until there is no more error
+        UI.errorMessageBox.runApp(creditsList)
+        creditsList = UI.creditsInput.runApp(courseDisplayName)
+    
+    if checkBack(creditsList):
+    	return inputStage - 1
+
+    for credit in creditsList:
+        c.execute('''UPDATE courses SET credits = ?;''',(credit,))      #adds to each course record the number of credits
+
+    return inputStage + 1
+
+def inputUnitFees(c, inputStage):
+	c.execute("SELECT DISTINCT program FROM students;")
+	programList = c.fetchall()
+
+	unitFeesList = UI.feeUnitsInput.runApp(programList)
+	while not checkError(unitFeesList) and not len(programList) == len(unitFeesList):
+		UI.errorMessageBox.runApp(unitFeesList)
+		unitFeesList = UI.feeUnitsInput.runApp(programList)
+
+	if checkBack(unitFeesList):
+		return inputStage - 1
+
+	if len(programList) == len(unitFeesList):
+		for i in range(len(unitFeesList)):
+
+			c.execute("UPDATE program_info SET unit_fees = ? WHERE program_name = ?;",(unitFeesList[i], programList[i][0],))
+	return inputStage + 1
+
+def inputBIU(c, inputStage):
+	BIUList = UI.BIUInput.runApp()
+	while not checkError(BIUList) and not len(BIUList) == 1:
+		UI.errorMessageBox.runApp(BIUList)
+		BIUList = UI.BIUInput.runApp()
+	if checkBack(BIUList):
+		return inputStage - 1
+
+	if len(BIUList) == 1:
+
+		c.execute("INSERT INTO constants (name,value) VALUES (?,?);",("BIU Value", BIUList[0]))
+	return inputStage + 1
+
+def inputFormulaFees(c, inputStage):
+	c.execute("SELECT DISTINCT program FROM students;")
+	programList = c.fetchall()
+
+	formulaFeeList = UI.formulaFeesInput.runApp(programList)
+	while not checkError(formulaFeeList) and not len(programList) == len(formulaFeeList):
+		UI.errorMessageBox.runApp(formulaFeeList)
+		formulaFeeList = UI.formulaFeesInput.runApp(programList)
+
+	if checkBack(formulaFeeList):
+		return inputStage - 1 
+
+	if len(programList) == len(formulaFeeList):
+		for i in range(len(formulaFeeList)):
+
+			c.execute("UPDATE program_info SET formula_fees = ? WHERE program_name = ?;",(formulaFeeList[i],programList[i][0]))
+
+	return inputStage + 1
+
+def inputNormalUnits(c, inputStage):
+	c.execute("SELECT DISTINCT program FROM students;")
+	programList = c.fetchall()
+
+	normalUnitsList = UI.normalUnitsInput.runApp(programList)
+	while not checkError(normalUnitsList) and not len(programList) == len(normalUnitsList):
+		UI.errorMessageBox.runApp(normalUnitsList)
+		normalUnitsList = UI.normalUnitsINput.runApp(programList)
+
+	if checkBack(normalUnitsList):
+		return inputStage - 1 
+
+
+	if len(programList) == len(normalUnitsList):
+		for i in range(len(normalUnitsList)):
+			c.execute("UPDATE program_info SET normal_units = ? WHERE program_name = ?;", (normalUnitsList[i],programList[i][0],))
+
+	return inputStage + 1
+
+def inputProgWeights(c, inputStage):
+	c.execute("SELECT DISTINCT program FROM students;")
+	programList = c.fetchall()
+
+	programList.append("1st Year Arts")
+	programList.append("1st Year Science")
+
+	progWeightsList = UI.programWeightsInput.runApp(programList)
+	
+	while not checkError(progWeightsList) and not len(programList) == len(progWeightsList):
+		UI.errorMessageBox.runApp(progWeightsList)
+		progWeightsList = UI.programWeightsInput.runApp(programList)
+
+	if checkBack(progWeightsList):
+		return inputStage - 1 
+
+	if len(programList) == len(progWeightsList):
+		for i in range(len(progWeightsList[:-2])):		#SQL updates for all programs "BSCH", "BAH" but NOT 1st year arts and science
+
+			c.execute("UPDATE program_info SET program_weight = ? WHERE program_name = ?;",(progWeightsList[i],programList[i][0]))
+			
+		for i in range(len(progWeightsList)-2,len(progWeightsList)):
+
+			c.execute("SELECT COUNT (*) FROM program_info WHERE program_name = ?", (programList[i],))
+			checkExists = c.fetchone()
+
+			if checkExists == (0,):		#happens when 1st year Arts and Science records DO NOT exist
+				
+				c.execute("INSERT INTO program_info(program_name, program_weight) VALUES (?,?);",(programList[i],progWeightsList[i]))
+			
+			elif checkExists == (1,):	#happens when 1st year Arts and Science records DO exist
+				
+				c.execute("UPDATE program_info SET program_weight = ? WHERE program_name = ?;", (progWeightsList[i],programList[i]))
+
+	return inputStage + 1
+
+
+'''Input the credits using the entry widget (creditsInput module) 
+'''
+
+'''Insert auxiliary tables that contain information for processing
+- Unit Fees
+- BIU/Formula Fee
+- Program Weights
+
+'''
+
+def runApp():
+
+	conn = data.connectDB()
+
+	c = conn.cursor()
+
+	numOfFrames = 6
+
+	inputStage = 0 		#initialize
+	
+	while inputStage != numOfFrames:
+
+
+		if inputStage == 0:
+
+			inputStage = inputCredits(c,inputStage)
+
+		elif inputStage == 1:
+
+			inputStage = inputUnitFees(c,inputStage)
+
+		elif inputStage == 2:
+
+			inputStage = inputBIU(c,inputStage)
+
+		elif inputStage == 3:
+
+			inputStage = inputFormulaFees(c,inputStage)
+
+		elif inputStage == 4:
+
+			inputStage = inputNormalUnits(c,inputStage)
+
+		elif inputStage == 5:
+
+			inputStage = inputProgWeights(c,inputStage)
+
+		elif inputStage == -1:	#when it is equal to 0 (go back on inputCredits)
+
+			inputStage = 0  #makes them input credits again
+
+		else:
+			break 		
+
+	conn.commit()
+
+	conn.close()
+
+if __name__ == '__main__':
+	runApp()
+	

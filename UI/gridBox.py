c7d131e178e06686e449953f912d529de3d693bc
daniel <daniel.kao.95@gmail.com>
2014-08-08 11:09:50 -0400

    removed creditsInput (nonscrolling) from program

--- UI/gridBox.py
+++ UI/gridBox.py
@@ -1,3 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        gridbox output box
+
+# Purpose:		will output a two dimensional list in the form of a grid
+#
+# Author:      DBMS
+#
+# Created:     06/08/2014
+# Copyright:   (c) DBMS 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import xlwt
+
 from Tkinter import *
 data = [['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\ANAT 215 - Fall 2013.xls', u'ANAT', u' 215', 2139], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\ANAT 216 - Winter 2014.xls', u'ANAT', u' 216', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\ANAT 315 - Fall 2013.xls', u'ANAT', u' 315', 2139], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\ANAT 316 - Winter 2014.xls', u'ANAT', u' 316', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\BCHM 218 - Winter 2014.xls', u'BCHM', u' 218', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\BCHM 310 - Winter 2014.xls', u'BCHM', u' 310B', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\BCHM 313 - Winter 2014.xls', u'BCHM', u' 313', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\BCHM 315 - Fall 2013.xls', u'BCHM', u' 315', 2139], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\BCHM 316 - Winter 2014.xls', u'BCHM', u' 316', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\MICR 221 - Winter 2014.xls', u'MICR', u' 221', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHAR 100 (ONLINE) - Summer 2013.xls', u'PHAR', u' 100', 2135], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHAR 100 (ONLINE) - Winter 2014.xls', u'PHAR', u' 100', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHAR 100 - Fall 2013.xls', u'PHAR', u' 100', 2139], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHAR 340 - Winter 2014.xls', u'PHAR', u' 340', 2139], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHAR 450 - Fall 2013.xls', u'PHAR', u' 450', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHGY 210 (ONLINE) - Summer 2013.xls', u'PHGY', u' 210', 2135], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHGY 210 (ONLINE) - Winter 2014.xls', u'PHGY', u' 210', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHGY 214 - Winter 2014.xls', u'PHGY', u' 214B', 2141], ['C:\\Users\\DBMS\\Documents\\Daniel\\DBMS_Enrollment\\full_data\\PHGY 350 - Fall 2013.xls', u'PHGY', u' 350', 2139]]
 
